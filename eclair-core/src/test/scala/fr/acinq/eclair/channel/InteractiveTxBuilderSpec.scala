012e92dc0ba1058dd4c9afeacbd8f9a72cb440ba
t-bast <bastuc@hotmail.fr>
2022-05-20 10:49:53 +0200

    Unlock utxos at startup
    We remove utxos locks that were previously set, otherwise we may have
    utxos that stay locked indefinitely. This may happen if we started funding
    a channel and restarted before completing the funding flow: the channel
    will be safely forgotten but the corresponding utxos stay locked.
    
    The only drawback that this may have is if we have funded and signed a
    funding transaction but didn't publish it and we accidentally double-spend
    it after a restart. This shouldn't be an issue because:
    
    - locks are automatically removed when the transaction is published anyway
    - funding transactions are republished at startup if they aren't in the
      blockchain or in the mempool
    - funding transactions detect when they are double-spent and abort
      the channel creation
    - the only case where double-spending a funding transaction causes  a loss
      of funds is when we accept a 0-conf channel and our peer double-spends it,
      but we should never accept 0-conf channels from peers we don't trust

--- eclair-core/src/test/scala/fr/acinq/eclair/channel/InteractiveTxBuilderSpec.scala
+++ eclair-core/src/test/scala/fr/acinq/eclair/channel/InteractiveTxBuilderSpec.scala
@@ -21,7 +21,7 @@ import akka.actor.typed.scaladsl.adapter.{ClassicActorSystemOps, actorRefAdapter
 import akka.pattern.pipe
 import akka.testkit.TestProbe
 import fr.acinq.bitcoin.scalacompat.Crypto.PublicKey
-import fr.acinq.bitcoin.scalacompat.{ByteVector32, ByteVector64, Satoshi, SatoshiLong, Script, ScriptWitness, Transaction, TxOut}
+import fr.acinq.bitcoin.scalacompat.{ByteVector32, ByteVector64, OutPoint, Satoshi, SatoshiLong, Script, ScriptWitness, Transaction, TxOut}
 import fr.acinq.eclair.blockchain.OnChainWallet.{FundTransactionResponse, SignTransactionResponse}
 import fr.acinq.eclair.blockchain.bitcoind.BitcoindService
 import fr.acinq.eclair.blockchain.bitcoind.rpc.BitcoinCoreClient.{MempoolTx, Utxo}
@@ -217,10 +217,14 @@ class InteractiveTxBuilderSpec extends TestKitBaseClass with AnyFunSuiteLike wit
 
       // Utxos are locked for the duration of the protocol.
       val probe = TestProbe()
-      val locksA = getLocks(probe, rpcClientA)
+      val bitcoinClientA = new BitcoinCoreClient(rpcClientA)
+      bitcoinClientA.listLockedOutpoints().pipeTo(probe.ref)
+      val locksA = probe.expectMsgType[Set[OutPoint]]
       assert(locksA.size == 3)
       assert(locksA == Set(inputA1, inputA2, inputA3).map(toOutPoint))
-      val locksB = getLocks(probe, rpcClientB)
+      val bitcoinClientB = new BitcoinCoreClient(rpcClientB)
+      bitcoinClientB.listLockedOutpoints().pipeTo(probe.ref)
+      val locksB = probe.expectMsgType[Set[OutPoint]]
       assert(locksB.size == 1)
       assert(locksB == Set(toOutPoint(inputB1)))
 
@@ -462,7 +466,11 @@ class InteractiveTxBuilderSpec extends TestKitBaseClass with AnyFunSuiteLike wit
       alice ! Start(alice2bob.ref, Nil)
       assert(alice2bob.expectMsgType[LocalFailure].cause == ChannelFundingError(aliceParams.channelId))
       // Utxos shouldn't be locked after a failure.
-      awaitCond(getLocks(probe, rpcClientA).isEmpty, max = 10 seconds, interval = 100 millis)
+      val bitcoinClientA = new BitcoinCoreClient(rpcClientA)
+      awaitAssert({
+        bitcoinClientA.listLockedOutpoints().pipeTo(probe.ref)
+        assert(probe.expectMsgType[Set[OutPoint]].isEmpty)
+      }, max = 10 seconds, interval = 100 millis)
     }
   }
 
@@ -527,9 +535,11 @@ class InteractiveTxBuilderSpec extends TestKitBaseClass with AnyFunSuiteLike wit
       // Unusable utxos should be skipped.
       legacyTxIds.foreach(txid => assert(!txA.signedTx.txIn.exists(_.outPoint.txid == txid)))
       // Only used utxos should be locked.
-      awaitCond({
-        val locks = getLocks(probe, rpcClientA)
-        locks == txA.signedTx.txIn.map(_.outPoint).toSet
+      val bitcoinClientA = new BitcoinCoreClient(rpcClientA)
+      awaitAssert({
+        bitcoinClientA.listLockedOutpoints().pipeTo(probe.ref)
+        val locks = probe.expectMsgType[Set[OutPoint]]
+        assert(locks == txA.signedTx.txIn.map(_.outPoint).toSet)
       }, max = 10 seconds, interval = 100 millis)
     }
   }
